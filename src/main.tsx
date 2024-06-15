cb4ee0a079c2e2fda18477b117eaf54931f1241d
Guilherme Gomes Zucco <g62ucc0@hotmail.com>
2024-06-15 15:44:58 -0300

    add header component

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './routes'
 import './main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Header from './components/header'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <div className="default">
-    <React.StrictMode>    
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>     
+    <React.StrictMode>
+      
+      <Header />
+      
+      <div className='appBody'>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+      </div>
+
     </React.StrictMode>
 </div>
 )
