bc72b3802d4d50829223ffdf29c0e30cb6fd7256
Guilherme Gomes Zucco <g62ucc0@hotmail.com>
2024-06-15 15:50:19 -0300

    add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>      
+        <h6 className="text-center">GNU General Public License</h6>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,18 @@ import './main.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Header from './components/header'
+import Footer from './components/footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <div className="default">
     <React.StrictMode>
-      
-      <Header />
-      
+      <Header />      
       <div className='appBody'>
         <BrowserRouter>
             <Router />
         </BrowserRouter>
       </div>
-
+      <Footer />
     </React.StrictMode>
 </div>
 )
